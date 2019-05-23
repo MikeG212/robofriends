1031f9ab750fcb4d63c9f868debc9963ecf5f9d5
MikeG212 <michaelcgee93@gmail.com>
2019-05-23 13:23:14 -0700

    Create search bar

--- src/robots.js
+++ src/robots.js
@@ -1,16 +1,61 @@
 export const robots = [
     {
-        id: 1,
-        name: "Michael Jordan",
-        email: "goat@mail.com"
+        id: 4,
+        name: "Christian Laettner",
+        email: "christian@mail.com"
     },
     {
-        id: 2,
+        id: 5,
+        name: "David Robinson",
+        email: "david@mail.com"
+    },
+    {
+        id: 6,
+        name: "Patrick Ewing",
+        email: "hoyadestroya@georgetown.edu"
+    },
+    {
+        id: 7,
         name: "Larry Bird",
-        email: "larry@mail.com"
+        email: "larrylegend@mail.com"
     },
     {
-        id: 3,
+        id: 8,
+        name: "Scottie",
+        email: "scottie@mail.com"
+    },
+    {
+        id: 9,
+        name: "Michael Jordan",
+        email: "hisairness@mail.com"
+    },
+    {
+        id: 10,
+        name: "Clyde Drexler",
+        email: "clyde@mail.com"
+    },
+    {
+        id: 11,
+        name: "Karl Malone",
+        email: "karl@mail.com"
+    },
+    {
+        id: 12,
+        name: "John Stockton",
+        email: "john@mail.com"
+    },
+    {
+        id: 13,
+        name: "Chris Mullin",
+        email: "chris@runtmc.com"
+    },
+    {
+        id: 14,
+        name: "Charles Barkley",
+        email: "notarolemodel@mail.com"
+    },
+    {
+        id: 15,
         name: "Magic Johnson",
         email: "magic@mail.com"
     },
