191dd72235954d9f73f19121654636e863d6b2ef
MikeG212 <michaelcgee93@gmail.com>
2019-05-22 17:33:06 -0700

    Seed robots file

---
+++ src/robots.js
@@ -0,0 +1,17 @@
+export const robots = [
+    {
+        id: 1,
+        name: "Michael Jordan",
+        email: "goat@mail.com"
+    },
+    {
+        id: 2,
+        name: "Larry Bird",
+        email: "larry@mail.com"
+    },
+    {
+        id: 3,
+        name: "Magic Johnson",
+        email: "magic@mail.com"
+    },
+]
