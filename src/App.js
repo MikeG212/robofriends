b5ae46c4ccace9f49513876c436f494a1a1bc44c
MikeG212 <michaelcgee93@gmail.com>
2019-05-23 14:23:00 -0700

    Add scrolling

--- src/App.js
+++ src/App.js
@@ -2,15 +2,22 @@ import React from 'react';
 import CardList from './CardList'
 import SearchBox from './SearchBox'
 import { robots } from './robots';
+import Scroll from './scroll';
 import './App.css';
 
 class App extends React.Component {
     constructor() {
         super();
         this.state = {
-            robots: robots,
+            robots: [],
             searchfield: ''
         };
+    }
+
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(response => response.json())
+            .then(users => this.setState({ robots: users }));
     }
 
     onSearchChange = (e) => {
@@ -21,13 +28,20 @@ class App extends React.Component {
         const filteredRobots = this.state.robots.filter(robots => {
             return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
         })
-        return (
-            <div className='tc' >
-                <h1 className='f2'>Robofriends</h1>
-                <SearchBox searchChange={this.onSearchChange} />
-                <CardList robots={filteredRobots} />
-            </div>
-        );
+        if (this.state.robots.length === 0) {
+            return <h1>Loading...</h1>
+        }
+        else {
+            return (
+                <div className='tc' >
+                    <h1 className='f2'>Robofriends</h1>
+                    <SearchBox searchChange={this.onSearchChange} />
+                    <Scroll>
+                        <CardList robots={filteredRobots} />
+                    </Scroll>
+                </div>
+            );
+        }
     }
 }
 
